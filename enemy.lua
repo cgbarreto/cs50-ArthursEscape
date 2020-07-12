e81ae3faf640ba4a3e4c176bac58644237ceb854
Carlos Barreto <cglbarreto@gmail.com>
2020-07-12 15:35:05 -0300

    Enemy debug flag added

--- enemy.lua
+++ enemy.lua
@@ -1,13 +1,15 @@
 Enemy = Object:extend()
 
+debug_enemy = false
+
 function Enemy:new(image)
     --self.image = image
 
     self.image = love.graphics.newImage("graphics/enemies/hell-hound-run.png")
     self.imageWidth = self.image:getWidth()
     self.imageHeight = self.image:getHeight()
-    self.scaleFactorX = 2.1
-    self.scaleFactorY = 2.1
+    self.scaleFactorX = 2.3
+    self.scaleFactorY = 2.3
 
     self.width = 67
     self.height = 32
@@ -17,7 +19,7 @@ function Enemy:new(image)
     self.y = love.graphics.getHeight() - self.imageHeight * self.scaleFactorY - 15
     self.yInicial = self.y
 
-    self.runSpeed = 100
+    self.runSpeed = 300
 
     self.frames = {}
 
@@ -46,27 +48,28 @@ function Enemy:draw()
     --love.graphics.draw(self.image, self.frames[math.floor(self.currentFrame)],self.x, self.y, 0, self.scaleFactorX, self.scaleFactorY)
     love.graphics.draw(self.image, self.frames[math.floor(self.currentFrame)],self.x, self.y, 0, self.scaleFactorX, self.scaleFactorY)
 
-    love.graphics.print("self.y: " .. math.floor(self.y) .. "  self.yInicial: " .. self.yInicial, 100, 150)
-    love.graphics.print("self.currentFrame: " .. self.currentFrame, 100, 250)
-    --love.graphics.print("self.jumpSpeed: " .. math.floor(self.jumpSpeed), 100, 200)
+    if debug_enemy then 
+        love.graphics.print("self.y: " .. math.floor(self.y) .. "  self.yInicial: " .. self.yInicial, 100, 150)
+        love.graphics.print("self.currentFrame: " .. self.currentFrame, 100, 250)
+        --love.graphics.print("self.jumpSpeed: " .. math.floor(self.jumpSpeed), 100, 200)
 
-    ------ Study hit box
-    r,g,b,a = love.graphics.getColor()
-    love.graphics.setColor(1,0,0)
-    love.graphics.rectangle("line",self.x, self.y, self.width * self.scaleFactorX, self.height * self.scaleFactorY)
-    --love.graphics.setColor(r,g,b,a)
-    
-    love.graphics.setColor(0,1,0)
-    love.graphics.rectangle("line", self.x + self.width/2, self.y + self.height/2.7, 
-                            self.width * self.scaleFactorX - self.width/1.5, self.height * self.scaleFactorY - self.height/2.5)
-    love.graphics.setColor(r,g,b,a)
+        ------ Study hit box
+        r,g,b,a = love.graphics.getColor()
+        love.graphics.setColor(1,0,0)
+        love.graphics.rectangle("line",self.x, self.y, self.width * self.scaleFactorX, self.height * self.scaleFactorY)
+        --love.graphics.setColor(r,g,b,a)
+        
+        love.graphics.setColor(0,1,0)
+        love.graphics.rectangle("line", self.x + self.width/2, self.y + self.height/2.7, 
+                                self.width * self.scaleFactorX - self.width/1.5, self.height * self.scaleFactorY - self.height/2.5)
+        love.graphics.setColor(r,g,b,a)
 
-    --Slide hitbox
-    --if self.slideFlag == 1 then
-    --    love.graphics.setColor(0,0,1)
-    --    love.graphics.rectangle("line",self.x + 50, self.y + 65, self.width * self.scaleFactorX - 90, self.height * self.scaleFactorY - 65)
-    --   love.graphics.setColor(r,g,b,a)
-    --end
-
+        --Slide hitbox
+        --if self.slideFlag == 1 then
+        --    love.graphics.setColor(0,0,1)
+        --    love.graphics.rectangle("line",self.x + 50, self.y + 65, self.width * self.scaleFactorX - 90, self.height * self.scaleFactorY - 65)
+        --   love.graphics.setColor(r,g,b,a)
+        --end
+    end
 end
 
